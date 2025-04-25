7044562ebc6b2a8164e5efbc1550b6d968d48beb
Joshua Rashtian <joshrashtian1@gmail.com>
2025-04-24 21:00:46 -0700

    Added navbar, front page changes

---
+++ app/(components)/navbar.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { Chocolate_Classical_Sans, DM_Sans } from "next/font/google";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Directory = {
+  name: string;
+  href: string;
+};
+
+export const Directory: Directory[] = [
+  { name: "Home", href: "/" },
+
+  { name: "Gallery", href: "/gallery" },
+];
+
+const font = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
+
+const NavBar = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="absolute top-0 left-0 h-24 w-full  flex items-center justify-center">
+      <nav className="flex items-center shadow-2xl bg-zinc-400/50 backdrop-blur-sm justify-center gap-4 w-fit h-16 rounded-full">
+        {Directory.map((item) => {
+          const isActive = pathname.split("/")[1] === item.href.split("/")[1];
+          return (
+            <Link
+              href={item.href}
+              key={item.name}
+              className={`${font.className} py-4 px-8 ${
+                isActive
+                  ? "bg-white scale-110   text-zinc-900"
+                  : "text-zinc-900 "
+              }  text-2xl rounded-full hover:bg-zinc-400 hover:text-white hover:scale-105 drop-shadow-2xl duration-500`}
+            >
+              <p
+                className={`${
+                  isActive ? "text-zinc-400" : "text-zinc-900"
+                } hover:scale-105 duration-500`}
+              >
+                {item.name}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { MdGamepad, MdWeb } from "react-icons/md";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold font-mono">
+        College of the Canyons' Tech Club
+      </h1>
+      <p className="text-2xl">
+        Where Developers at College of the Canyons all come together.
+      </p>
+      <div className="flex items-center justify-center gap-4">
+        <section className="text-6xl bg-zinc-200/50 flex-col flex items-center justify-center gap-4 backdrop-blur-sm rounded-lg p-4">
+          <MdGamepad />
+          <p className="text-2xl">Game Development</p>
+        </section>
+        <section className="text-6xl bg-zinc-200/50 flex-col flex items-center justify-center gap-4 backdrop-blur-sm rounded-lg p-4">
+          <MdWeb />
+          <p className="text-2xl">Web Development</p>
+        </section>
+      </div>
     </div>
   );
 }
